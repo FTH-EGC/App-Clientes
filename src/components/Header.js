ddd40546450ef91512148c43b6926a76f4cf836f
e.g.c <ironmaiden-erick@hotmail.com>
2020-12-24 18:21:58 -0600

    Agregando estilos al header y agregando clase header para estilos globales

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,28 +5,36 @@ import styled from '@emotion/styled';
 
 const SiteHeader = styled.header`
     display: grid;
-    grid-template-columns: repeat(2,1fr);
+    grid-template-rows: repeat(2, auto);
     color: #ffffff;
     text-transform: uppercase;
-    font-size: 1.5rem;
-    font-weight: 700;
+    font-size: 1rem;
+    font-weight: 900;
+    margin-top: 4rem;
+
+    @media (min-width: 768px){
+        font-size: 1.2rem;
+        grid-template-rows: unset;
+        grid-template-columns: repeat(2,max-content);
+        align-items: center;
+    }
 
     img{
         max-width: 100%;
+        height: 5rem;
     }
-
-
-
 `;
 
 
 
 const Header = () => {
     return ( 
-        <SiteHeader className="contenedor">
-            <img src={Diente} alt="Logo"/>
-            <h1>Administrador de Citas</h1>
-        </SiteHeader>
+        <div className="header">
+            <SiteHeader className="contenedor">
+                <img src={Diente} alt="Logo"/>
+                <h1>Administrador de Citas</h1>
+            </SiteHeader>
+        </div>
 
      );
 }
