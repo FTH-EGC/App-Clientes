32af4ec8eedaa703173f56d5269b1db998796a03
e.g.c <ironmaiden-erick@hotmail.com>
2020-12-24 19:08:24 -0600

    Agregando inputs al formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import styled from '@emotion/styled';
 
+const Form = styled.form`
 
+
+`;
 
 
 
@@ -14,6 +17,40 @@ const Formulario = () => {
         <form 
         
         >
+            <label htmlFor="nombre">Nombre</label>
+            <input
+                type="text"
+                name="nombre"
+                placeholder="Tu Nombre"
+
+            />
+            <label htmlFor="edad">Edad</label>
+            <input
+                type="text"
+                name="edad"
+                placeholder="Tu Edad"
+            />
+            <label htmlFor="telefono">Teléfono</label>
+            <input
+                type="tel"
+                name="telefono"
+                placeholder="Tu Teléfono"
+                
+            />
+            <label htmlFor="fecha">Fecha</label>
+            <input
+                type="date"
+                name="fecha"
+            />
+            <label htmlFor="hora">Hora</label>
+            <input
+                type="time"
+                name="hora"           
+            />
+            <label htmlFor="sintomas">Sintomas</label>
+            <textarea 
+            name="sintomas"
+            ></textarea>
 
         </form>
      );
