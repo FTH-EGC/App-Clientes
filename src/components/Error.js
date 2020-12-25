a0b9911007f0496cf5b57506ba4c45e2f2346615
e.g.c <ironmaiden-erick@hotmail.com>
2020-12-24 20:48:10 -0600

    Agregando estilos al error

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Parrafo = styled.p`
+    color: #ed1c5b;
+    font-size: 1.4rem;
+    font-weight: 900;
+    text-align: center;
+    text-transform: uppercase;
+`;
 
 
 const Error = () => {
     return ( 
-        <p>Todos los campos son obligatorios</p>
+        <Parrafo>Todos los campos son obligatorios</Parrafo>
      );
 }
  
