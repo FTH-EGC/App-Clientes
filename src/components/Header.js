8c1c1fc561223b855045e885b3db27b37f30f3fe
e.g.c <ironmaiden-erick@hotmail.com>
2020-12-24 18:09:03 -0600

    Agregando estilos al Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,31 @@ import React from 'react'
 import Diente from '../assets/img/Diente.svg';
 import styled from '@emotion/styled';
 
+
+const SiteHeader = styled.header`
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    color: #ffffff;
+    text-transform: uppercase;
+    font-size: 1.5rem;
+    font-weight: 700;
+
+    img{
+        max-width: 100%;
+    }
+
+
+
+`;
+
+
+
 const Header = () => {
     return ( 
-        <header className="header contenedor">
+        <SiteHeader className="contenedor">
             <img src={Diente} alt="Logo"/>
             <h1>Administrador de Citas</h1>
-        </header>
+        </SiteHeader>
 
      );
 }
