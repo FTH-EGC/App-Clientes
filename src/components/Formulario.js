1cc250ba2348e5327dd1b059f124baf200464641
e.g.c <ironmaiden-erick@hotmail.com>
2020-12-24 19:46:08 -0600

    Quitando PlaceHolders

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -28,12 +28,16 @@ const Inputs = styled.input`
     &:focus{
         border-bottom: 1px solid #541dd6;
         outline: none;
+        -webkit-box-shadow: 0px 11px 17px -7px rgba(0,0,0,0.56);
+        -webkit-box-shadow: 0px 11px 17px -10px rgba(0,0,0,0.56);
+        -moz-box-shadow: 0px 11px 17px -10px rgba(0,0,0,0.56);
+        box-shadow: 0px 11px 17px -10px rgba(0,0,0,0.56);
     }
 `;
 
 const Labels = styled.label`
-    margin: 1rem;
-
+    margin: 1rem 0 .5rem 0;
+    font-size: 1.2rem;
 `;
 
 
@@ -50,20 +54,17 @@ const Formulario = () => {
             <Inputs
                 type="text"
                 name="nombre"
-                placeholder="Tu Nombre"
 
             />
             <Labels htmlFor="edad">Edad</Labels>
             <Inputs
                 type="text"
                 name="edad"
-                placeholder="Tu Edad"
             />
             <Labels htmlFor="telefono">Teléfono</Labels>
             <Inputs
                 type="tel"
                 name="telefono"
-                placeholder="Tu Teléfono"
                 
             />
             <Labels htmlFor="fecha">Fecha</Labels>
