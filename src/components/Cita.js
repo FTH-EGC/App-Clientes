4669ae03771511339cbfb019051bf98f0094fc29
e.g.c <ironmaiden-erick@hotmail.com>
2020-12-25 18:07:06 -0600

    Finalizando estilos de las citas

--- src/components/Cita.js
+++ src/components/Cita.js
@@ -2,42 +2,73 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 
+
 const CardCita = styled.div`
     background-color: #ffffff;
     padding: 1rem;
     display: grid;
     grid-template-rows: repeat(2, auto);
-    width: 80%;
+    width: 85%;
     margin-bottom: 2rem;
     border-radius: 30px;
 
     @media (min-width: 768px){
-        width: 60%
+        width: 70%
     }
+
     p{
         font-size: 1.2rem;
-        font-weight: 900;
+        font-weight: 700;
     }
     span{
-        font-size: 1rem;
         font-weight: 400;
     }
+
 
 `;
 
 const DatosFecha = styled.div`
     display: flex;
     justify-content: space-between;
-
-    p{
-        flex-basis: calc(50% - 1rem);
-        text-align: center;
-    }
-    span{
-        text-align: right;
-    }
+    border-bottom: 1px solid #707070;
 
 `;
+
+const Botones = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    column-gap: 4rem;
+    margin-bottom: 1rem;
+`;
+const BotonEditar = styled.button`
+    background-color: #0073CF;
+    color: #ffffff;
+    border: none;
+    padding: .5rem;
+    border-radius: 5px;
+
+`;
+
+const BotonEliminar = styled.button`
+    background-color: #E20303;
+    color: #ffffff;
+    border: none;
+    padding: .5rem;
+    border-radius: 5px;
+
+`;
+
+const Sintomas = styled.div`
+    padding: .5rem;
+    border: 1px solid #707070;
+    margin-bottom: 1rem;
+    border-radius: 10px;
+
+
+
+`;
+
+
 
 const Cita = ({paciente}) => {
 
@@ -54,16 +85,18 @@ const Cita = ({paciente}) => {
                 <p>Nombre: <span>{nombre}</span></p>
                 <p>Edad: <span>{edad}</span></p>
                 <p>Telefono: <span>{telefono}</span></p>
-                <div className="sintomas">
-                    <p>{sintomas}</p>
-                </div>
-                <div className="botones">
-                    <button
+                <p>Sintomas:</p>
+                <Sintomas>
+                    <span>{sintomas}</span>
+                </Sintomas>
+                <Botones>
+                    <BotonEditar
                         name="editar"
-                    >Editar</button>
-                    <button
-                    >Eliminar</button>
-                </div>
+                    >Editar</BotonEditar>
+
+                    <BotonEliminar
+                    >Eliminar</BotonEliminar>
+                </Botones>
             </div>
         </CardCita>
      );
